d40ad714912a07a6598df1e5ee3a25a9556da15d
Laura Jabr <lmj7@rice.edu>
2022-02-22 00:45:59 -0600

    finished plotting

--- EDA.py
+++ EDA.py
@@ -12,14 +12,20 @@ df_test_y = pd.DataFrame(test_y) + 1
 df_train_y = pd.DataFrame(train_y) + 1
 df_test_x = pd.DataFrame(test_x)
 
+df_test_y.rename(columns={0: 'Rating'}, inplace=True)
+df_train_y.rename(columns={0: 'Rating'}, inplace=True)
+
 # print(df_train_y[0].unique(), df_test_y[0].unique())
 # print(df_train_y.mode() + 1)
 # print(df_test_y.mode() + 1)
-# print(df_test_y.median())
+print(df_train_y.std())
+
+df_train_y = df_train_y["Rating"].astype(int).astype('category')
+df_test_y = df_test_y["Rating"].astype(int).astype('category')
 
 plt.style.use('ggplot')
-df_train_y.value_counts().plot(kind = "barh")
+df_test_y.value_counts().sort_index().plot(kind = "barh", color = "navy")
 plt.ylabel("Sentiment Rating")
 plt.xlabel("Number of Sentences")
-plt.title("Sentiment Rating Distribution In Training Set", loc = "left")
+plt.title("Sentiment Rating Distribution In Testing Set", loc = "left")
 plt.show()
