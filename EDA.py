801228497dc496641dc3866085d98ca7f000e0bb
Laura Jabr <lmj7@rice.edu>
2022-02-21 22:26:51 -0600

    EDA starting plots

--- EDA.py
+++ EDA.py
@@ -1,3 +1,25 @@
-# Exploratory data analysis
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle as pk
 
-print(11)
+train_x = pk.load(open( "./data/train_x.pk", "rb" ))
+train_y = pk.load(open( "./data/train_y.pk", "rb" ))
+
+test_x = pk.load(open( "./data/test_x.pk", "rb" ))
+test_y = pk.load(open( "./data/test_y.pk", "rb" ))
+
+df_test_y = pd.DataFrame(test_y) + 1
+df_train_y = pd.DataFrame(train_y) + 1
+df_test_x = pd.DataFrame(test_x)
+
+# print(df_train_y[0].unique(), df_test_y[0].unique())
+# print(df_train_y.mode() + 1)
+# print(df_test_y.mode() + 1)
+# print(df_test_y.median())
+
+plt.style.use('ggplot')
+df_train_y.value_counts().plot(kind = "barh")
+plt.ylabel("Sentiment Rating")
+plt.xlabel("Number of Sentences")
+plt.title("Sentiment Rating Distribution In Training Set", loc = "left")
+plt.show()
