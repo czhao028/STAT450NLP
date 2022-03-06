2711d8ca0d4b29dce31781d8e314ee9e6f6189c1
Christine <czhao028@gmail.com>
2022-03-06 16:39:26 -0600

    preprocess text stop words code

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ We exclusively use conda environments for this project. To recreate ours, run `c
 Next, run `conda activate STAT450NLP` in the command line (cmd on Windows) to activate the conda environment. If working in Pycharm, use [this link](https://www.jetbrains.com/help/pycharm/conda-support-creating-conda-virtual-environment.html) to connect the existing Conda environment as an interpreter.
 
 Finally, you'll need to install **pytreebank** to use the Stanford Treebank Dataset (under the folder **sst**) functions in Python. With the conda environment activated in either cmd or Pycharm, run `conda install pip` then `pip install pytreebank`. This extra step is because *pytreebank* and others are examples of libraries only offered through pip.
+
+## Updating the Project
+
+To make sure conda environments are consistent across laptops, make sure to run 
+1. `conda list --explicit > spec-file.txt` whenever you install a new package
+2. `conda install --name STAT450NLP --file spec-file.txt` whenever you pull from Github. This updates your environment with all the packages that've been installed by others.
--- preprocess_sst.py
+++ preprocess_sst.py
@@ -1,6 +1,41 @@
 import sst.load_sst
 import pickle as pk
 import re
+import nltk
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+import pandas as pd
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+
+from sklearn.preprocessing import LabelEncoder
+from collections import Counter
+from sklearn.metrics import classification_report,confusion_matrix,accuracy_score
+from keras.models import Sequential
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
+lemmatizer = WordNetLemmatizer()
+def cleaning(train_x_list, stop_words):
+    #train_x_list.apply(lambda x:' '.join(x.lower() for x in x.split())) already lowered in load_sst.py
+    for sentence in train_x_list:
+        # Replacing the special characters
+        new_sent = re.sub('[^ws]', '', sentence)
+        # Replacing the digits/numbers
+        new_sent = re.sub('d', '', new_sent)
+        # Removing stop words
+        all_tokens = list()
+        for token in new_sent.split():
+            if token in stop_words:
+                continue
+            all_tokens.append(lemmatizer.lemmatize(token))
+        return ' '.join(all_tokens)
+
+
+
 
 train_sst = sst.load_sst.get_train()
 train_x = list(train_sst.keys())
@@ -13,3 +48,5 @@ test_x = list(test_sst.keys())
 test_y = list(test_sst.values())
 pk.dump(test_x, open( "./data/test_x.pk", "wb" ))
 pk.dump(test_y, open( "./data/test_y.pk", "wb" ))
+
+
