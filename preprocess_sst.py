6639d91e6f87ec19a2dbdc591b74dc6a7a9962f4
Christine <czhao028@gmail.com>
2022-03-06 17:05:45 -0600

    cleaned pk

--- preprocess_sst.py
+++ preprocess_sst.py
@@ -2,7 +2,8 @@ import sst.load_sst
 import pickle as pk
 import re
 import nltk
-nltk.download('wordnet')
+#nltk.download('wordnet')
+#nltk.download('stopwords')
 from nltk.stem import WordNetLemmatizer
 import pandas as pd
 from nltk.corpus import stopwords
@@ -23,9 +24,9 @@ def cleaning(train_x_list, stop_words):
     #train_x_list.apply(lambda x:' '.join(x.lower() for x in x.split())) already lowered in load_sst.py
     for sentence in train_x_list:
         # Replacing the special characters
-        new_sent = re.sub('[^ws]', '', sentence)
-        # Replacing the digits/numbers
-        new_sent = re.sub('d', '', new_sent)
+        new_sent = re.sub(r'[^\w\s](([dts]|ll|ve|re)\s)*', '', sentence)
+        # # Replacing the digits/numbers
+        # new_sent = re.sub(r'\d', '', new_sent)
         # Removing stop words
         all_tokens = list()
         for token in new_sent.split():
@@ -34,19 +35,18 @@ def cleaning(train_x_list, stop_words):
             all_tokens.append(lemmatizer.lemmatize(token))
         return ' '.join(all_tokens)
 
-
-
-
 train_sst = sst.load_sst.get_train()
-train_x = list(train_sst.keys())
+uncleaned_train_x = list(train_sst.keys())
+train_x = cleaning(uncleaned_train_x, stop_words)
 train_y = list(train_sst.values())
 pk.dump(train_x, open( "./data/train_x.pk", "wb" ))
 pk.dump(train_y, open( "./data/train_y.pk", "wb" ))
+pk.dump(uncleaned_train_x, open( "./data/train_x_uncleaned.pk", "wb" ))
 
 test_sst = sst.load_sst.get_test()
-test_x = list(test_sst.keys())
+uncleaned_test_x = list(test_sst.keys())
+test_x = cleaning(uncleaned_test_x, stop_words)
 test_y = list(test_sst.values())
 pk.dump(test_x, open( "./data/test_x.pk", "wb" ))
 pk.dump(test_y, open( "./data/test_y.pk", "wb" ))
-
-
+pk.dump(uncleaned_test_x, open( "./data/test_x_uncleaned.pk", "wb" ))
